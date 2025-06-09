a434152cdc97e7049062756197216b48aa1f51c6
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-06-09 19:54:59 +0300

    feat: set up user routes and error handling middlewares

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ import express from "express";
 import { connect } from "mongoose";
 
 import authRouter from "./routes/auth.js";
+import userRouter from "./routes/user.js";
 
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
 
 app.use("/api/v1/", authRouter);
+app.use("/api/v1/", userRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Source location not found" });
+});
+app.use((err, req, res, next) => {
+  res
+    .status(500)
+    .json({ message: "Internal server error!", error: err.message });
+});
 
 const PORT = process.env.PORT || 8080;
 const MONGODB_URI = process.env.MONGODB_URI;
