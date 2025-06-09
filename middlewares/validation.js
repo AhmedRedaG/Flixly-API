1bd15f10d6d2605ccc21561552940f46a57926bd
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-06-09 18:03:28 +0300

    feat: add validation middleware for user registration and login

---
+++ middlewares/validation.js
@@ -0,0 +1,41 @@
+import { body, validationResult } from "express-validator";
+
+export const name = body("name", "Name must be valid.")
+  .trim()
+  .isLength({ min: 3, max: 256 })
+  .matches(/^[A-Za-z\s]+$/);
+
+export const email = body("email", "Email must be valid")
+  .isEmail()
+  .normalizeEmail();
+
+export const password = body(
+  "password",
+  "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+)
+  .trim()
+  .isStrongPassword()
+  .isLength({ max: 64 });
+
+export const confirmPassword = body(
+  "confirmPassword",
+  "Passwords must by the same."
+)
+  .trim()
+  .custom((value, { req }) => {
+    if (value !== req.body.password) return false;
+    return true;
+  });
+
+const result = (req, res, next) => {
+  const validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    const errors = {};
+    validationErrors.array().forEach((err) => (errors[err.path] = err.msg));
+    return res.status(422).json({ message: "Validation failed", errors });
+  }
+
+  next();
+};
+
+export default result;
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,26 @@ import { Router } from "express";
 
 import { postRegister, postLogin } from "../controllers/auth.js";
 
+import validationResult, * as validation from "../middlewares/validation.js";
+
 const router = Router();
 
-router.post("/register", postRegister);
+router.post(
+  "/register",
+  [
+    validation.name,
+    validation.email,
+    validation.password,
+    validation.confirmPassword,
+    validationResult,
+  ],
+  postRegister
+);
 
-router.post("/login", postLogin);
+router.post(
+  "/login",
+  [validation.email, validation.password, validationResult],
+  postLogin
+);
 
 export default router;
