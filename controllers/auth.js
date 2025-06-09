eca2b1f1affd25a8b07dce915f9d84f25ae4d26a
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-06-09 09:28:34 +0300

    feat: implement user registration with password hashing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,2 +1,22 @@
-export const postRegister = (req, res, next) => {};
+import bcrypt from "bcrypt";
+
+import User from "../models/user.js";
+
+export const postRegister = async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({ name, email, password: hashedPassword });
+    const savedUser = await user.save();
+    const { password: _, ...userSafeData } = savedUser.toObject();
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully", data: userSafeData });
+  } catch (err) {
+    next(err);
+  }
+};
+
 export const postLogin = (req, res, next) => {};
