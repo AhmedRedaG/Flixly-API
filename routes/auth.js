9eb39d2adc323b81a2b29f82044a5105c92ac0a7
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-06-10 22:03:16 +0300

    feat: add express-rate-limit middleware to enhance security by limiting attempts

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,13 @@ import { Router } from "express";
 import * as authController from "../controllers/auth.js";
 
 import validationResult, * as validation from "../middlewares/isValid.js";
+import rateLimiter from "../middlewares/rateLimiter.js";
 
 const router = Router();
 
 router.post(
   "/register",
+  rateLimiter,
   [
     validation.name,
     validation.email,
@@ -20,11 +22,12 @@ router.post(
 
 router.post(
   "/login",
+  rateLimiter,
   [validation.email, validation.password, validationResult],
   authController.postLogin
 );
 
-router.post("/refresh", authController.postRefresh);
+router.post("/refresh", rateLimiter, authController.postRefresh);
 
 router.post("/logout", authController.postLogout);
 
