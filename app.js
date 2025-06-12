9a1618b7dd5e075c06f054da385e69d88a80630b
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-06-12 19:05:11 +0300

    feat: add cors and helmet middleware for enhanced security and request handling

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import express from "express";
 import { connect } from "mongoose";
 import cookieParser from "cookie-parser";
 import jsendMiddleware from "jsend-middleware";
+import helmet from "helmet";
+import cors from "cors";
 
 import authRouter from "./routes/auth.js";
 import userRouter from "./routes/user.js";
@@ -10,8 +12,10 @@ import userRouter from "./routes/user.js";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+app.use(cors());
+app.use(helmet());
 app.use(cookieParser());
+app.use(express.json());
 app.use(jsendMiddleware());
 
 app.use("/api/v1/auth/", authRouter);
