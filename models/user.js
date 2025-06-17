698b9cdab1cdc2fa552f64e53ae5d402ddf3338d
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-06-17 23:30:33 +0300

    feat: add resetToken field to user schema for password reset functionality

--- models/user.js
+++ models/user.js
@@ -18,6 +18,7 @@ const userSchema = new Schema(
     },
     role: { type: String, default: "user" },
     refreshTokens: [String],
+    resetToken: { type: String },
   },
   {
     timestamps: true,
