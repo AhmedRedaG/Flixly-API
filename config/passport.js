62672372cbd9b61c97705205ad5517e6bbe43dbb
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-06-17 03:49:13 +0300

    feat: implement user find or creation logic in Google authentication strategy

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,22 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 
+import User from "../models/user.js";
+
+const findOrCreateUser = async (profile) => {
+  const oldUser = await User.findOne({ googleId: profile.id });
+  if (oldUser) {
+    return oldUser;
+  }
+
+  const newUser = await new User({
+    name: profile.displayName,
+    email: profile.emails[0].value,
+    googleId: profile.id,
+  }).save();
+  return newUser;
+};
+
 passport.use(
   new GoogleStrategy(
     {
@@ -8,6 +24,13 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/api/v1/auth/google/callback",
     },
-    () => {}
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const user = await findOrCreateUser(profile);
+        done(user, null);
+      } catch (err) {
+        done(err, null);
+      }
+    }
   )
 );
