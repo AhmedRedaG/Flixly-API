bc024dcef3cf828aaa7d782c7e6c4332f4451b8a
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-06-17 05:39:10 +0300

    refactor: enhance user schema by ensuring unique and formatted email, and adjusting googleId properties

--- models/user.js
+++ models/user.js
@@ -3,8 +3,19 @@ import { Schema, model } from "mongoose";
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String },
+    googleId: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
     role: { type: String, default: "user" },
     refreshTokens: [String],
   },
