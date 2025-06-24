cefe41c8af5318e43fc6966282defa658f33e0d9
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-06-24 18:40:52 +0300

    feat: add phoneNumber and OTP fields to user schema

--- models/user.js
+++ models/user.js
@@ -19,6 +19,8 @@ const userSchema = new Schema(
     role: { type: String, default: "user" },
     refreshTokens: [String],
     resetToken: { type: String },
+    phoneNumber: { type: Number },
+    OTP: { type: Boolean, default: false },
   },
   {
     timestamps: true,
