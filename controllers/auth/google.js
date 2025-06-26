6082a9ec14e4eb5800a404f116dc4bd95debcff8
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-06-26 03:30:58 +0300

    feat: enhance Google authentication flow with user validation and TFA handling

--- controllers/auth/google.js
+++ controllers/auth/google.js
@@ -5,6 +5,12 @@ import CookieHelper from "../../utilities/cookieHelper.js";
 export const authWithGoogle = async (req, res, next) => {
   const userGoogleId = req.user; // google id only
   const user = await User.findOne({ googleId: userGoogleId });
+  if (!user) return res.jsend.fail({ googleId: "Invalid googleId" }, 401);
+
+  if (user.TFA.status === true) {
+    const tempToken = JwtHelper.createTempToken({ _id: user._id });
+    return res.jsend.fail({ phoneNumber: user.phoneNumber, tempToken }, 401);
+  }
 
   const userSafeData = JwtHelper.getSafeData(user);
   const refreshToken = JwtHelper.createRefreshToken(userSafeData);
