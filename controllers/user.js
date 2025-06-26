4095dfd153a2ad9385f29e1239690fc73d2f9be5
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-06-26 05:56:32 +0300

    feat: add setPhoneNumber route to update user's phone number with validation

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,26 @@
+import { getUserByIdOrFail } from "../utilities/dbHelper";
+
 // for testing JWT
 export const getUser = (req, res, next) => {
   const user = req.user;
 
   res.jsend.success({ user });
+};
+
+export const setPhoneNumber = async (req, res, next) => {
+  const { phoneNumber } = req.body;
+
+  const user = getUserByIdOrFail(req.user._id);
+  if (!user) return;
+
+  // cant reset number with 2fa enabled, you can disable it first or update the number using the 2fa code
+  if (user.TFA.number && user.TFA.status === true)
+    return res.jsend.fail({
+      phoneNumber: "Phone number already set and 2fa is activated",
+    });
+
+  user.TFA.number = phoneNumber;
+  await user.save();
+
+  res.jsend.success({ phoneNumber });
 };
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 
-import { getUser } from "../controllers/user.js";
+import { getUser, setPhoneNumber } from "../controllers/user.js";
+import validationResult, * as validation from "../middlewares/isValid.js";
 
 import isAuth from "../middlewares/isAuth.js";
 
 const router = Router();
 
-router.get("/users/", isAuth, getUser);
+router.get("/users", isAuth, getUser);
+
+router.put(
+  "/users/phone-number",
+  isAuth,
+  [validation.phoneNumber, validationResult],
+  setPhoneNumber
+);
 
 export default router;
