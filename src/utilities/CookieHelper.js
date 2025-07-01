8ee7789f814decd4e1c09525da8f3c18441181af
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-07-01 23:00:25 +0300

    refactor: use constant for refresh token age in createRefreshTokenCookie function

--- src/utilities/CookieHelper.js
+++ src/utilities/CookieHelper.js
@@ -1,10 +1,12 @@
-export function createRefreshTokenCookie(res, refreshToken) {
+const REFRESH_TOKEN_AGE = 1000 * 60 * 60 * 24 * 7; // 7 days
+
+export function createRefreshTokenCookie(refreshToken, res) {
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax",
     secure: process.env.NODE_ENV === "production",
     path: "/api/v1/auth",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    maxAge: REFRESH_TOKEN_AGE,
   });
 }
 
