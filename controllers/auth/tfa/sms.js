429093190da2858d67de6f3fb392bfbec55508ab
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-07-01 16:57:52 +0300

    refactor: SMS verification code logic into service layer

--- controllers/auth/tfa/sms.js
+++ controllers/auth/tfa/sms.js
@@ -1,39 +1,10 @@
-import crypto from "crypto";
-import { sendTFASms } from "../../../utilities/smsSender.js";
-import { getUserByIdOrFail } from "../../../utilities/dbHelper.js";
+import * as smsServer from "../../../services/auth/tfa/sms.service.js";
 
 const TFA_DURATION = 1000 * 60 * 5; // 5 minutes
 const SMS_DURATION = 1000 * 60 * 15; // 15 minutes
 
 export const sendSmsVerificationCode = async (req, res) => {
-  const user = await getUserByIdOrFail(req.user._id, res);
-  if (!user) return;
-
-  if (
-    user.TFA.sms.lastSentAt &&
-    user.TFA.sms.lastSentAt > new Date(Date.now() - SMS_DURATION)
-  ) {
-    return res.jsend.fail(
-      { message: "Can't send SMS right now, try again later" },
-      429
-    );
-  }
-
-  const TFACode = crypto.randomInt(100000, 999999);
-  const TFAExpiredAt = new Date(Date.now() + TFA_DURATION);
-  const phoneNumber = user.TFA.sms.number;
-
-  // await sendTFASms(phoneNumber, TFACode);
-  console.log(">>>>>>>>>>>> " + TFACode);
-
-  user.TFA.sms.code = TFACode;
-  user.TFA.sms.expiredAt = TFAExpiredAt;
-  user.TFA.sms.lastSentAt = new Date();
-  user.TFA.sms.attempts = 0;
-  await user.save();
-
-  res.jsend.success({
-    message: `2FA sent successfully to *******${phoneNumber.slice(9)}`,
-    TFAExpiredAt,
-  });
+  const userId = req.user._id;
+  const data = smsServer.sendSmsVerificationCodeService(userId);
+  res.jsend.success(data);
 };
---
+++ services/auth/tfa/sms.service.js
@@ -0,0 +1,35 @@
+import crypto from "crypto";
+import { sendTFASms } from "../../../utilities/smsSender.js";
+import { getUserByIdOrFail } from "../../../utilities/dbHelper.js";
+
+const TFA_DURATION = 1000 * 60 * 5; // 5 minutes
+const SMS_DURATION = 1000 * 60 * 15; // 15 minutes
+
+export const sendSmsVerificationCodeService = async (userId) => {
+  const user = await getUserByIdOrFail(userId);
+
+  if (
+    user.TFA.sms.lastSentAt &&
+    user.TFA.sms.lastSentAt > new Date(Date.now() - SMS_DURATION)
+  ) {
+    throw new AppError("Can't send SMS right now, try again later", 429);
+  }
+
+  const TFACode = crypto.randomInt(100000, 999999);
+  const TFAExpiredAt = new Date(Date.now() + TFA_DURATION);
+  const phoneNumber = user.TFA.sms.number;
+
+  // await sendTFASms(phoneNumber, TFACode);
+  console.log(">>>>>>>>>>>> " + TFACode);
+
+  user.TFA.sms.code = TFACode;
+  user.TFA.sms.expiredAt = TFAExpiredAt;
+  user.TFA.sms.lastSentAt = new Date();
+  user.TFA.sms.attempts = 0;
+  await user.save();
+
+  return {
+    phoneNumber: `*******${phoneNumber.slice(9)}`,
+    TFAExpiredAt,
+  };
+};
