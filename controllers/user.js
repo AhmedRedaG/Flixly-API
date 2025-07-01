cc76f5b2904b1b30862d7569cfa18d73fee93f9b
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-07-01 19:03:07 +0300

    refactor: remove setPhoneNumber function from user controller

--- controllers/user.js
+++ controllers/user.js
@@ -6,21 +6,3 @@ export const getUser = (req, res, next) => {
 
   res.jsend.success({ user });
 };
-
-export const setPhoneNumber = async (req, res, next) => {
-  const { phoneNumber } = req.body;
-
-  const user = await getUserByIdOrFail(req.user._id);
-  if (!user) return;
-
-  // cant reset number with 2fa enabled, you can disable it first or update the number using the 2fa code
-  if (user.TFA.number && user.TFA.status === true)
-    return res.jsend.fail({
-      phoneNumber: "Phone number already set and 2fa is activated",
-    });
-
-  user.TFA.number = phoneNumber;
-  await user.save();
-
-  res.jsend.success({ phoneNumber });
-};
--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,10 @@
 import { Router } from "express";
 
-import { getUser, setPhoneNumber } from "../controllers/user.js";
-import validationResult, * as validation from "../middlewares/isValid.js";
-
+import { getUser } from "../controllers/user.js";
 import isAuth from "../middlewares/isAuth.js";
 
 const router = Router();
 
 router.get("/users", isAuth, getUser);
-
-router.put(
-  "/users/phone-number",
-  isAuth,
-  [validation.phoneNumber, validationResult],
-  setPhoneNumber
-);
 
 export default router;
