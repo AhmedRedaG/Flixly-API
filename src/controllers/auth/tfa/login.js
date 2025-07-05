0d653d1f2b5a7d668cf1d57818835ef4ab55ff5c
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-07-05 20:05:32 +0300

    fix: remove backupCode from TFA verification

--- src/controllers/auth/tfa/login.js
+++ src/controllers/auth/tfa/login.js
@@ -2,15 +2,10 @@ import * as loginServer from "../../../services/auth/tfa/login.service.js";
 import * as CookieHelper from "../../../utilities/cookieHelper.js";
 
 export const loginVerifyTFA = async (req, res) => {
-  const { TFACode, method, backupCode } = req.body;
+  const { TFACode, method } = req.body;
   const userId = req.user._id;
   const { accessToken, refreshToken, userSafeData } =
-    await loginServer.loginVerifyTFAService(
-      userId,
-      TFACode,
-      method,
-      backupCode
-    );
+    await loginServer.loginVerifyTFAService(userId, TFACode, method);
   CookieHelper.createRefreshTokenCookie(refreshToken, res);
   res.jsend.success({ accessToken, user: userSafeData });
 };
