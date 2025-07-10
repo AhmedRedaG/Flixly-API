36a6beca4156f03ab7d6f6d4a49da4b516cc4512
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-07-10 20:03:41 +0300

    refactor: update google auth endpoints response with messages

--- src/services/auth/google.service.js
+++ src/services/auth/google.service.js
@@ -9,12 +9,21 @@ export const authWithGoogleService = async (googleId) => {
 
   if (user.TFA.status === true) {
     const tempToken = JwtHelper.createTempToken({ _id: user._id });
-    return { method: user.TFA.method, tempToken };
+    return {
+      method: user.TFA.method,
+      tempToken,
+      message: "Two-factor authentication required",
+    };
   }
 
   const { accessToken, refreshToken, userSafeData } =
     await generateTokensForUser(user);
   await user.save();
 
-  return { accessToken, refreshToken, userSafeData };
+  return {
+    accessToken,
+    refreshToken,
+    userSafeData,
+    message: "Google login successful",
+  };
 };
