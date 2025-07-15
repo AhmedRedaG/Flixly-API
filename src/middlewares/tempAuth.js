7ff7e001a4198730778fa3afe70cebac605bf894
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-07-16 00:47:07 +0300

    refactor: improve temporary token extraction and validation from authorization header`

--- src/middlewares/tempAuth.js
+++ src/middlewares/tempAuth.js
@@ -2,8 +2,17 @@ import * as JwtHelper from "../utilities/jwtHelper.js";
 import AppError from "../utilities/appError.js";
 
 const tempAuth = async (req, res, next) => {
-  const { tempToken } = req.body;
-  if (!tempToken) throw new AppError("Missing temp token", 422);
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader) {
+    throw new AppError("Authorization header is missing", 401);
+  }
+
+  if (!authorizationHeader.startsWith("Bearer ")) {
+    throw new AppError("Invalid Authorization format", 401);
+  }
+
+  const tempToken = authorizationHeader.split(" ")[1];
 
   req.user = JwtHelper.verifyTempToken(tempToken);
 
