9c64a503b7cd0a80e38ab01af183146b521d3777
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-08-02 07:41:16 +0300

    feat: implement playlists migration with Sequelize

---
+++ database/migrations/20250802044008-create-playlists.js.js
@@ -0,0 +1,58 @@
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("playlists", {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+      channel_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: "channels",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      is_private: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("playlists");
+  },
+};
