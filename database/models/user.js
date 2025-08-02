89cfc2a8e63dcc2b8b87d578867e2d23851bb0c0
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-08-02 08:04:00 +0300

    feat: enhance User model associations and add role field

--- database/models/user.js
+++ database/models/user.js
@@ -3,14 +3,58 @@ import { DataTypes, Model } from "sequelize";
 export default (sequelize) => {
   class User extends Model {
     static associate(models) {
+      // User has one channel
+      User.hasOne(models.Channel, {
+        foreignKey: "user_id",
+        as: "channel",
+      });
+
+      // User has many refresh tokens
       User.hasMany(models.RefreshToken, {
         foreignKey: "user_id",
         as: "refreshTokens",
       });
 
+      // User has one reset token
       User.hasOne(models.ResetToken, {
         foreignKey: "user_id",
         as: "resetToken",
+      });
+
+      // User has many video reactions
+      User.hasMany(models.VideoReaction, {
+        foreignKey: "user_id",
+        as: "videoReactions",
+      });
+
+      // User has many video views
+      User.hasMany(models.VideoView, {
+        foreignKey: "user_id",
+        as: "videoViews",
+      });
+
+      // User has many comments
+      User.hasMany(models.VideoComment, {
+        foreignKey: "user_id",
+        as: "videoComments",
+      });
+
+      // User has many subscriptions (as subscriber)
+      User.hasMany(models.Subscription, {
+        foreignKey: "subscriber_id",
+        as: "subscriptions",
+      });
+
+      // User has many playlists
+      User.hasMany(models.Playlist, {
+        foreignKey: "user_id",
+        as: "playlists",
+      });
+
+      // User has many reports (as reporter)
+      User.hasMany(models.Report, {
+        foreignKey: "reporter_id",
+        as: "reports",
       });
     }
   }
@@ -39,6 +83,10 @@ export default (sequelize) => {
       verified: { type: DataTypes.BOOLEAN, defaultValue: false },
       avatar: DataTypes.STRING,
       bio: DataTypes.STRING,
+      role: {
+        type: DataTypes.ENUM("user", "admin"),
+        defaultValue: "user",
+      },
     },
     {
       sequelize,
