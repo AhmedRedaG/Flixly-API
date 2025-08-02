1dbb673b9c9c1f2fa58ca241795b3da29e755430
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-08-02 08:24:39 +0300

    feat: implement videoView model associations and update migrations

---
+++ database/models/videoView.js
@@ -0,0 +1,52 @@
+import { Model } from "sequelize";
+
+export default (sequelize, DataTypes) => {
+  class VideoView extends Model {
+    static associate(models) {
+      VideoView.belongsTo(models.User, {
+        foreignKey: "user_id",
+        as: "user",
+      });
+
+      VideoView.belongsTo(models.Video, {
+        foreignKey: "video_id",
+        as: "video",
+      });
+    }
+  }
+
+  VideoView.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      user_id: {
+        type: DataTypes.UUID,
+      },
+      video_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      watch_time: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      watched_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: "VideoView",
+      tableName: "video_views",
+      timestamps: false,
+      indexes: [{ fields: ["video_id"] }],
+    }
+  );
+
+  return VideoView;
+};
