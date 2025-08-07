318cc718d71486743476dd61efa7341d3c027a2a
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-08-07 09:59:07 +0300

    chore: add cloudinary and multer dependencies to package.json and package-lock.json and setup configurations

---
+++ config/multer.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) cb(null, true);
+  else cb(null, false);
+};
+
+const upload = multer({ storage, fileFilter });
+
+export default upload;
