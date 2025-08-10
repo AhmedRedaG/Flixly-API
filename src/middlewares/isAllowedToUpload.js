6a25e885b1d9fdfdaca926c3ab8a92c76e701fb3
Ahmed Reda <ahmedrf.dev@gmail.com>
2025-08-10 05:39:57 +0300

    refactor: implement upload validation middleware for video and image uploads, ensuring proper error handling and user permissions

---
+++ src/middlewares/isAllowedToUpload.js
@@ -0,0 +1,45 @@
+import AppError from "../utilities/appError.js";
+
+export const isUserUploadVideoAllowed = async (req, res, next) => {
+  const { user } = req;
+  const { videoId } = req.params;
+
+  const channel = await user.getChannel();
+  if (!channel) throw new AppError("Channel not found", 404);
+
+  const [video] = await channel.getVideos({ where: { id: videoId }, limit: 1 });
+  if (!video) throw new AppError("Video not found", 404);
+
+  if (video.processing_status === "completed")
+    throw new AppError("Video already uploaded", 409);
+  if (video.processing_status === "processing")
+    throw new AppError("Video is being processed", 409);
+
+  next();
+};
+
+export const isUserUploadImageAllowed = async (req, res, next) => {
+  const { user } = req;
+  const { processId } = req.params;
+  const { type } = req.query;
+
+  if (type === "userAvatar") {
+    if (user.id !== processId) throw new AppError("User not found", 404);
+  }
+
+  const channel = await user.getChannel();
+
+  if (type === "channelAvatar" || type === "channelBanner") {
+    if (!channel || channel.id !== processId)
+      throw new AppError("Channel not found", 404);
+  }
+
+  if (type === "thumbnail") {
+    if (!channel) throw new AppError("Channel not found", 404);
+
+    const [video] = channel.getVideos({ where: processId, limit: 1 });
+    if (!video) throw new AppError("Video not found");
+  }
+
+  next();
+};
